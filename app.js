7fb6ac9d94cbe0ff1fa9f7cb67460efc48d600da
Vera Chernushina <chernushina.vera@gmail.com>
2022-05-24 01:48:59 +0300

    feat: create schemas and models

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3005 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log('Server is started at port 3005');
